4f6e5e50fd8946ed42a40546298c911a2bf85a9a
peterhenrique <pedrohsdeoliveira27@gmail.com>
2022-08-07 11:30:39 -0300

    lib/

--- lib/balanced.rb
+++ lib/balanced.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 class Node
 
     attr_accessor :data, :left, :right
@@ -39,8 +41,44 @@ class Tree
     end
 
     def delete(value, node = @root)
-        until value == node.data 
+        p node.data
+        if value < node.data
+            node.left.left.nil? ? node.left = node.left.right : delete(value, node.left)
+        elsif value > node.data
+            node.right.right.nil? ? node.right = node.right.left : delete(value, node.right)
+        else
+            if !node.left.nil? && !node.right.nil?
+                temp2 = node.left
+                temp = minValueNode(node.right)
+                node.data = temp.data
+                node.right = temp.right
+                node.left = temp2            
+            end        
+        end  
+        
+    end
 
+    def minValueNode(node)
+        current = node
+        while current.left != nil
+            current = current.left
+        end
+        current
+    end
+
+    def pretty_print(node = @root, prefix = '', is_left = true)
+        pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+        puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+        pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    end
+
+    def find(value, node = @root)
+        if value == node.data
+            node
+        elsif value < node.data
+            find(value, node.left)
+        else
+            find(value, node.right)
         end
     end
 
@@ -51,13 +89,29 @@ end
 
 k = Tree.new([1,2,3,5,6,7,8])
 
-k.insert(9)
-
-p k 
-
+#k.insert(9)
+#k.pretty_print
 
 
- #p Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
+
+#k.delete(1)
+#k.delete(6)
+
+#k.pretty_print
+
+
+j = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+p j.pretty_print
+
+#j.delete(67)
+
+p j.find(67)
+
+
+
+
+
 
 #j = Tree.new([1])
 
